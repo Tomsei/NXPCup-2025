b1ef7f2e824759a40cf4c61fd234d3d0390f8009
Tom <tom.seiffert@study.hs-duesseldorf.de>
2024-11-06 18:21:48 +0100

    add poti

---
+++ SW/PlatformIO/src/singleComponents/poti.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "configuration/globalConfig.h"
+
+void setupPoti() {
+    return;
+}
+
+void runPoti() {
+    int poti1 = analogRead(poty1);
+    int poti2 = analogRead(poty2);
+    if(poti1 > 500 || poti2 > 500) {
+        Serial.print("Poti 1: "); Serial.print(poti1);
+        Serial.print("\t Poti 2: "); Serial.println(poti2);
+    }
+}
