5ff974eead2ec0ee0b28155a23212ef9c84441fd
Tom <tom.seiffert@study.hs-duesseldorf.de>
2024-12-19 15:23:42 +0100

    add implementation to board Input Control for setup, update, ...

--- SW/PlatformIO/src/boardInput/boardInput.cpp
+++ SW/PlatformIO/src/boardInput/boardInput.cpp
@@ -3,14 +3,50 @@
 #include "Adafruit_MCP23X17.h"
 
 #include "configuration/globalConfig.h"
+
 namespace BoardInput {
 
-    Data data;
+    static Data data;
     Adafruit_MCP23X17 ioExpander;
     
+    void setupIoExpander();
+    void setupData();
     int readPoti(int poti);
+    int readButton(int button);
+    int readDipSwitch();
 
 
+    /* ToDo: Error Handling!*/
+
+    /*--------------- "Public" --------------- */
+    void setup() {
+        setupIoExpander();
+        setupData();
+    }
+
+    void update() {
+        data.poti1 = readPoti(POTI1);
+        data.poti2 = readPoti(POTI2);
+        data.button1 = readButton(IO_EX_BUTTON1);
+        data.button2 = readButton(IO_EX_BUTTON2);
+        data.dipSwitch = readDipSwitch();
+    }
+
+    void printData() {
+        String dataToPrint = "";
+        dataToPrint = dataToPrint + "Poti 1: " + data.poti1 + " Poti 2: " + data.poti2 
+        + " Button 1: " + data.button1 + " Button 2 " + data.button2 + " Dip Switch: " + data.dipSwitch;
+        Serial.print ("Board Input | ");
+        Serial.println(dataToPrint);
+    }
+
+    void getData() {
+        
+    }
+
+
+
+    /*--------------- "Privat" ---------------*/
     void setupIoExpander() {
         if(!ioExpander.begin_I2C()) {
             Serial.println("BoardInput: IO-Expander Init Error");
@@ -23,22 +59,25 @@ namespace BoardInput {
         ioExpander.pinMode(IO_EX_DIPSWITCH4, INPUT_PULLUP);
     }
 
-    void update() {
-        data.poti1 = readPoti(POTI1);
-        data.poti2 = readPoti(POTI2);
-        data.dipswitch = 5;
+    void setupData() {
+        data.poti1 = -1;
+        data.poti2 = -1;
+        data.button1 = -1;
+        data.button2 = -1;
+        data.dipSwitch = -1;
     }
-
 
     int readPoti(int poti) {
         return analogRead(poti);
     }
 
-    int readButton() {
-        return -1;
+    int readButton(int button) {
+        return ioExpander.digitalRead(button);
     }
 
     int readDipSwitch() {
-        return -1;
+        int dipSwitchvalue = 0;
+        //@ToDo: Implement
+        return dipSwitchvalue;
     }
 }
--- SW/PlatformIO/src/boardInput/boardInput.h
+++ SW/PlatformIO/src/boardInput/boardInput.h
@@ -6,7 +6,7 @@ namespace BoardInput {
     {
         uint16_t poti1;
         uint16_t poti2;
-        uint8_t dipswitch;
+        uint8_t dipSwitch;
         uint8_t button1;
         uint8_t button2;
     };
