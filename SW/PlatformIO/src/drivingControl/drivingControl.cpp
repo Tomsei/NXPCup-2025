c600bc8c1b829523fdb0eb5e5df26b57db0babc4
Tom <tom.seiffert@study.hs-duesseldorf.de>
2025-01-18 10:39:19 +0100

    comment driving control

--- SW/PlatformIO/src/drivingControl/drivingControl.cpp
+++ SW/PlatformIO/src/drivingControl/drivingControl.cpp
@@ -1,17 +1,29 @@
+/**
+ * driving control - definition
+ * 
+ * handels everything to control the car driving. 
+ * This includes functions to setup engine and servo and
+ * the general control of them to drive correctly.
+ * 
+ * @author Tom Seiffert
+ */
 #include "drivingControl/drivingControl.h"
 #include "drivingControl/engine.h"
 #include "drivingControl/servo.h"
 
+//ToDo: check where these variables should be implementet!
 extern bool engineEnabled;
 extern bool steeringEnabled;
 
 namespace DrivingControl {
-
+    
+    //comment in .h file
     void setup() {
         SteeringServo::setup();
         Engine::setup();
     }
 
+    //comment in .h file
     void drive(uint8_t speed, uint8_t steeringAngle) {
         if(engineEnabled) {
             Engine::setSpeed(speed);
