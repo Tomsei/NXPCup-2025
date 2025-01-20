56da907962d6ff379118b7d3e0a3149110bed859
Tom <tom.seiffert@study.hs-duesseldorf.de>
2025-01-20 15:40:02 +0100

    working test imu data struct

--- SW/PlatformIO/src/sensors/imu.h
+++ SW/PlatformIO/src/sensors/imu.h
@@ -30,13 +30,6 @@ namespace Sensors {
         void setup();
 
         /**
-         * get the Motion data and write it to 
-         * the variables the pointers points to
-         * @param ax, ay, az, gx, gy, gz: variable pointers to store the motion data
-         */
-        void getMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz);
-
-        /**
          * get the motion data and write it into the
          * data variable.
          * @param data: pointer to the data variable the values should be written
